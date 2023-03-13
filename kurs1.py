41409834e929ff4edbaa2a16457fb7aef71c18a2
AliGalip75 <galipyetis1111@gmail.com>
2023-03-13 20:45:56 +0300

    commit

---
+++ kurs1.py
@@ -0,0 +1,27 @@
+#*************Python veri türleri****************
+
+#int:Tam sayıları tutmak için kullanılır.
+#float:ondalıklı sayıları tutmak için kullanılır.
+#boolean:True veya False değerlerini tutar.
+#complex:Karmaşık sayıları tutar.
+#str:metinleri tutar.
+
+#*********Kodlama.io sitesinde değişken olarak kullanıldığını düşündüğüm veriler**********
+
+#int:Tarih ve kursun ilerleme yüzdesi
+#str:Ekranda gördüğümüz metinler
+
+#******Kodlama.io sitesinde şart blokları kullanıldığını düşündüğüm kısımlar ve bir örnek*******
+
+#Hesap giriş-çıkış işlemlerinde kullanıldığını düşünüyorum
+hesapAd="AliGalip"
+hesapSifre=2023
+hesapAdiGir=input("hesap adını gir:")
+hesapSifreGir=int(input("hesap şifresini gir:"))
+if hesapAdiGir==hesapAd and hesapSifreGir==hesapSifre:
+    print("Giriş başarılı")
+else:
+    print("Giriş başarısız")
+
+
+
